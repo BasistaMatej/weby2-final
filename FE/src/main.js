751bcaf9d42b0fa9b939bc9a80aafbdfc55a6312
Samuel Kubala <kubalasamuel1a@gmail.com>
2024-04-30 20:02:40 +0200

    FE-Registration-Complete

--- FE/src/main.js
+++ FE/src/main.js
@@ -7,11 +7,11 @@ import PrimeVue from 'primevue/config';
 import 'primevue/resources/themes/aura-light-purple/theme.css'
 import 'primevue/resources/primevue.min.css'
 import 'primeicons/primeicons.css'
-
+import ToastService from 'primevue/toastservice';
 
 const app = createApp(App)
 
 app.use(PrimeVue);
-app.use(router)
-
+app.use(router);
+app.use(ToastService);
 app.mount('#app')
--- FE/src/views/Registration.vue
+++ FE/src/views/Registration.vue
@@ -5,7 +5,8 @@
     <div>
         <DefaultNavBar />
         <div class="d-flex justify-content-center align-items-center flex-column w-100 p-3 h-100">
-            <h1 class="roboto-black">Registračný formulár_</h1>
+            <h1 class="roboto-black h1">REGISTÁCIA</h1>
+            <h4>NOVÝ POUŽÍVATEĽ</h4>
         </div>
     </div>
 
@@ -15,37 +16,46 @@
                 <InputGroupAddon>
                     <i class="pi pi-user"></i>
                 </InputGroupAddon>
-                <InputText placeholder="Name" name="name" />
+                <InputText v-model="name" :invalid="!isNameValid" placeholder="Name" name="name"
+                    @click="validateName" />
             </InputGroup>
 
             <InputGroup>
                 <InputGroupAddon>
                     <i class="pi pi-user"></i>
                 </InputGroupAddon>
-                <InputText placeholder="Surname" name="surname" />
+                <InputText v-model="surname" @click="validateSurname" :invalid="!isSurnameValid" placeholder="Surname"
+                    name="surname" />
             </InputGroup>
 
             <InputGroup>
                 <InputGroupAddon>
                     <i class="pi pi-at"></i>
                 </InputGroupAddon>
-                <InputText placeholder="Email" name="email" />
+                <InputText v-model="email" @click="validateEmail" :invalid="!isEmailValid" placeholder="Email"
+                    name="email" />
             </InputGroup>
 
             <InputGroup>
                 <InputGroupAddon>
                     <i class="pi pi-lock"></i>
                 </InputGroupAddon>
-                <Password v-model="password" :invalid="!passwordsMatch" placeholder="Password" toggleMask />
-            </InputGroup>
-
-            <InputGroup>
-                <InputGroupAddon>
-                    <i class="pi pi-lock"></i>
-                </InputGroupAddon>
-                <Password v-model="confirmPassword" :invalid="!passwordsMatch" placeholder="Confirm Password"
+                <Password v-model="password" @click="checkPasswords" :invalid="!passwordsMatch" placeholder="Password"
                     toggleMask />
             </InputGroup>
+
+            <InputGroup>
+                <InputGroupAddon>
+                    <i class="pi pi-lock"></i>
+                </InputGroupAddon>
+                <Password v-model="confirmPassword" @click="checkPasswords" :invalid="!passwordsMatch"
+                    placeholder="Confirm Password" toggleMask />
+            </InputGroup>
+            <Button @click="submitForm" type="submit" label="Registrácia">Registrovať <lord-icon
+                    src="https://cdn.lordicon.com/oqdmuxru.json" trigger="hover" colors="primary:#ffffff"
+                    style="width:2em;height:2em;margin-left:1em;">
+                </lord-icon></Button>
+            <Toast />
         </div>
     </div>
 
@@ -58,35 +68,131 @@ import InputGroup from 'primevue/inputgroup';
 import InputGroupAddon from 'primevue/inputgroupaddon';
 import InputText from 'primevue/inputtext';
 import Password from 'primevue/password';
+import Button from 'primevue/button';
+import Toast from 'primevue/toast';
+import { useToast } from 'primevue/usetoast';
 
 const password = ref(null)
 const confirmPassword = ref(null)
 const passwordsMatch = ref(true)
+const email = ref(null);
+const isEmailValid = ref(true);
+const name = ref(null)
+const isNameValid = ref(true)
+const surname = ref(null)
+const isSurnameValid = ref(true)
+const toast = useToast();
 
 watch([password, confirmPassword], () => {
-    if (password.value !== confirmPassword.value) {
-        //console.log("Passwords do not match!");
+    checkPasswords();
+})
+
+watch([email], () => {
+    validateEmail();
+})
+
+const checkPasswords = () => {
+    if (password.value !== confirmPassword.value || (!password.value && !confirmPassword.value)) {
         passwordsMatch.value = false;
     } else {
-        //console.log("Passwords match!");
         passwordsMatch.value = true;
     }
+}
+
+const validateEmail = () => {
+    const emailRegex = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
+    if (email.value) {
+        isEmailValid.value = emailRegex.test(email.value);
+    } else {
+        isEmailValid.value = false;
+    }
+}
+
+const validateName = () => {
+    const nameSurnameRegex = /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/g;
+    if (name.value) {
+        isNameValid.value = nameSurnameRegex.test(name.value);
+    } else {
+        isNameValid.value = false;
+    }
+}
+
+const validateSurname = () => {
+    const nameSurnameRegex = /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/g;
+    if (surname.value) {
+        isSurnameValid.value = nameSurnameRegex.test(surname.value);
+    } else {
+        isSurnameValid.value = false;
+    }
+}
+
+watch(name, () => {
+    validateName();
 })
+
+watch(surname, () => {
+    validateSurname();
+})
+
+const showSuccess = () => {
+    toast.add({ severity: 'success', summary: 'Success Message', detail: 'Form submitted ! To activate your account, please visit your email inbox and activate your account.', life: 5000 });
+};
+
+const showError = () => {
+    toast.add({ severity: 'error', summary: 'Error Message', detail: 'Form was not submitted !', life: 3000 });
+};
+
+const submitForm = async () => {
+    if (isEmailValid.value == true && isNameValid.value == true && isSurnameValid.value == true && passwordsMatch.value == true) {
+        const response = await fetch('https://node17.webte.fei.stuba.sk/final/registration.php', {
+            method: 'POST',
+            body: JSON.stringify({
+                name: name.value,
+                surname: surname.value,
+                email: email.value,
+                password: password.value
+            })
+        });
+
+        if (!response.ok) {
+            showError();
+        } else {
+            showSuccess();
+        }
+    }
+}
 </script>
 
 <style scoped>
+h4 {
+    text-decoration: underline dotted #8B5CF6aa;
+    color: #8B5CF6ee;
+}
+
 #box {
     width: 50%;
     box-sizing: border-box !important;
     border-radius: 10px;
-    border: 3px solid #8B5CF6;
+    box-shadow: 0 0.5em 1em #8B5CF6aa;
     display: flex;
     align-items: center;
     flex-direction: column;
+    margin-top: 3em;
 }
 
 .p-inputgroup {
     margin: 1rem !important;
     width: calc(100% - 2rem) !important;
+}
+
+Button {
+    border-radius: 1em;
+    margin-bottom: 1rem;
+}
+
+@media (max-width: 930px) {
+    #box {
+        width: 80%;
+    }
 }
 </style>
