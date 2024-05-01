11b74223e097d55105c9d6aab5506a0e4b861b58
Matej Bašista <basista.matej1@gmail.com>
2024-05-01 11:27:39 +0200

    FE - email validation

--- FE/src/router/index.js
+++ FE/src/router/index.js
@@ -16,6 +16,11 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/Registration.vue')
+    },
+    {
+      path: '/activate/:token',
+      name: 'Activate email',
+      component: () => import('../views/EmailValidationView.vue')
     }
   ]
 })
